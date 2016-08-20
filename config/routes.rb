ffe6d51d8bb6a164a2294708bdfc41c6c06c4349
Clifton Regis <github email address>
2016-08-20 02:37:40 -0400

    Completed lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :edit, :update, :show, :new, :create]
+  # get '/posts/:id/edit', to: 'posts#edit'
+  # patch 'posts/:id', to: 'posts#update'
 end
